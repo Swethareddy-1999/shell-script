e316654792972f163f63965bc456f81911157632
swetha <swethareddy01.com>
2025-12-29 17:40:27 +0530

    data added

--- 05.install-script.sh
+++ 05.install-script.sh
@@ -8,15 +8,23 @@ then
     exit 1 #other then 0
 fi
 
-yum install mysql -y
+yum list installed mysql
 
-if [ $? -eq 0 ] #$?privious output will give here as input
-then    
-    echo "Installing MYAQL ...failure"
-    exit 1
+if [$? -ne 0 ]
+then  #not installed
+    if [ $? -eq 0 ] #$?privious output will come here as input
+    then    
+        echo "Installing Mysql ...failure"
+        exit 1
+    else
+        echo "installing mysql ... success"
+    fi
 else
-    echo "installing mysql ... success"
+    echo "mysql is already ...installed"
 fi
+
+
+
 yum install git -y
 
 if [ $? -eq 0 ]
