72cae2f2c5878e0950b9f5416472a036491298e3
swetha <swethareddy01.com>
2025-12-29 16:18:34 +0530

    data added

--- 05.install-script.sh
+++ 05.install-script.sh
@@ -4,12 +4,26 @@ USERID=$(id -u)
 
 if [ $USERID -eq 0 ] #ne=not eqaul
 then 
-    echo "ERROR:: you must have non sudo access to excute this script"
+    echo "ERROR:: you must have sudo access to excute this script"
     exit 1 #other then 0
 fi
 
 yum install mysql -y
 
+if [ $? -eq 0 ] #$?privious output will give here as input
+then    
+    echo "Installing MYAQL ...failure"
+    exit 1
+else
+    echo "installing mysql ... success"
+fi
 yum install git -y
-yum install docker -y
-yum i
+
+if [ $? -eq 0 ]
+then    
+    echo "Installing git...failure"
+    exit 1
+else
+    echo "installing git ... success"
+fi        
+
