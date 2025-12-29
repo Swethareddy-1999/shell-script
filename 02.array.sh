4ac2312f05d58673d6f854d3394870abc6b0ba3d
swetha <swethareddy01.com>
2025-12-29 13:40:53 +0530

    adding the some data

---
+++ 02.array.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+
+NAMES=("minnu""swetha""minni")
+# index starts from 0, size is 3
+
+echo "first name: ${NAMES[0]}"
+echo "first name: ${NAMES[1]}"
+echo "first name: ${NAMES[2]}"
+
+echo "first name: ${NAMES[@]}"
