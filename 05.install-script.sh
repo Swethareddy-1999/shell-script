c32c25ce6372c8e15dd23f89f66b0ad6b9e6c4e8
swetha <swethareddy01.com>
2025-12-29 14:56:09 +0530

    data added

---
+++ 05.install-script.sh
@@ -0,0 +1,13 @@
+#!bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0] #ne=not eqaul
+then 
+    echo "ERROR:: you must have sudo access to excute this script"
+    exit 1 #other then 0
+fi
+
+dnf install mysql -y
+
+dnf install git -y
