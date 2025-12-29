93afe8c56357935fd6675d5b8e3ba4dd21216430
swetha <swethareddy01.com>
2025-12-29 13:20:34 +0530

    adding the some data

--- 01.variable.sh
+++ 01.variable.sh
@@ -4,4 +4,6 @@ NUMBER2=$2
 TIMESTAMP=$(date)
 echo "print the time":$TIMESTAMP
 SUM=$(($NUMBER1+$NUMBER2))
+MULTIPLE=$(($NUMBER1*$NUMBER2))
 echo "print the sum of $NUMBER1 & $NUMBER2 is":$SUM
+echo " print the multiple of $NUMBER1 and $NUMBER2 is":$MULTIPLE 
