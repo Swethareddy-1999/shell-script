32637a4b066996642486a03f111767ff9a78d729
swetha <swethareddy01.com>
2025-12-29 16:07:28 +0530

    data added

--- 05.install-script.sh
+++ 05.install-script.sh
@@ -4,7 +4,7 @@ USERID=$(id -u)
 
 if [ $USERID -e 0 ] #ne=not eqaul
 then 
-    echo "ERROR:: you must have sudo access to excute this script"
+    echo "ERROR:: you must have non sudo access to excute this script"
     exit 1 #other then 0
 fi
 
