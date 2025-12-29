61786e897095913f63d61a42060004f102748592
swetha <swethareddy01.com>
2025-12-29 15:21:49 +0530

    data added

--- 05.install-script.sh
+++ 05.install-script.sh
@@ -2,13 +2,14 @@
 
 USERID=$(id -u)
 
-if [ $USERID -ne 0 ] #ne=not eqaul
+if [ $USERID -e 0 ] #ne=not eqaul
 then 
     echo "ERROR:: you must have sudo access to excute this script"
-    #exit 1 #other then 0
+    exit 1 #other then 0
 fi
 
 yum install mysql -y
 
 yum install git -y
 yum install docker -y
+yum i
