d30c2269ea967eb948edfd86ce23c055135de579
swetha <swethareddy01.com>
2025-12-31 18:16:19 +0530

    data added

--- 9.loops.sh
+++ 9.loops.sh
@@ -25,7 +25,7 @@ echo "Script started excuting at: $TIMESTAMP" &>>$LOG_FILE_NAME
 for package in $@
 do 
     dnf list installed $package &>>$LOG_FILE_NAME
-    if [$? -ne 0]
+    if [ $? -ne 0 ]
     then
         dnf install $package -y &>>$LOG_FILE_NAME
         VALIDATE $? "Installing $package"
