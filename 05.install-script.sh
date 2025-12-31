700212f484e4b237e1546b2f9c1f1bd8434e424f
swetha <swethareddy01.com>
2025-12-31 13:43:49 +0530

    data added

--- 05.install-script.sh
+++ 05.install-script.sh
@@ -5,21 +5,22 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ] #ne=not eqaul
 then 
     echo "ERROR:: you must have sudo access to excute this script"
-    #exit 1 # is used for if above stmt is failed thn the excution have to stop here and it should be any num other then 0
+    exit 1 # is used for if above stmt is failed thn the excution have to stop here and it should be any num other then 0
 fi
 
-yum list installed mysql # it will get o/p sql is installed or not
+yum list installed mysql-server # it will get o/p sql is installed or not
 if [ $? -ne 0 ] # not installed mysql execute this stmnt
 then  
+    yum install mysql -y
     if [ $? -ne 0 ] # privious output will come here as input
     then    
-        echo "Installing Mysql ...failure"
+        echo "Installing Mysql-server ...failure"
         exit 1 
     else
-        echo "installing mysql ... success"
+        echo "installing mysql-server ... success"
     fi
 else # if sql instlled excute this stmt
-    echo "mysql is already ...installed"
+    echo "mysql-server is already ...installed"
 fi
 
 
