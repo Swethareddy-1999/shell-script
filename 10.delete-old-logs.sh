325d4b0f033b2df99b698585008c94aed23f3832
swetha <swethareddy01.com>
2026-01-06 14:09:23 +0530

    expense shell

--- 10.delete-old-logs.sh
+++ 10.delete-old-logs.sh
@@ -34,3 +34,8 @@ echo "Script started excuting at: $TIMESTAMP" &>>$LOG_FILE_NAME
 
 FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -mtime +14)
 echo "files to be deleted: $FILES_TO_DELETE" 
+
+while read -r file
+do
+    echo "DELETING file: $file"
+done <<< $FILES_TO_DELETE
