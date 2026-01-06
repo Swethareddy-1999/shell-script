e0d7b4483ab9e7fe1d370c5fa534a353ae7d3e80
swetha <swethareddy01.com>
2026-01-06 13:57:29 +0530

    expense shell

--- 10.delete-old-logs.sh
+++ 10.delete-old-logs.sh
@@ -7,7 +7,7 @@ y="\e[31m"
 
 SOURCE_DIR="/home/ec2-user/app-logs"
 
-LOGS_FOLDER="/var/log/shellscript-logs"
+LOGS_FOLDER="/var/log/expense-shell"
 LOG_FILE=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
